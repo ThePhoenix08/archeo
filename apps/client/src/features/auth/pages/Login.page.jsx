7e3308b86e20158809ce088357ee35000688c748
712Kunal <jadhavkun0@gmail.com>
2025-06-18 22:17:29 +0530

    updated app.jsx for adding role in the login and register route and also halfly done with the part of the login page

--- apps/client/src/features/auth/pages/Login.page.jsx
+++ apps/client/src/features/auth/pages/Login.page.jsx
@@ -1,5 +1,46 @@
+import React, { useState } from "react";
+import { useNavigate, useParams } from "react-router";
+
 function LoginPage() {
-  return <div>LoginPage</div>;
+	const navigate = useNavigate();
+	const role = useParams();
+
+	const validRoles = ["issuer", "verifier", "user"];
+	if (!validRoles.includes(role)) {
+		navigate("/");
+	}
+
+	const getFieldsForRoles = () => {
+		switch (role) {
+			case "issuer":
+				return [
+					{ name: "username", type: "text", label: "Username", required: true },
+					{ name: "email", type: "email", label: "Email", required: true },
+					{ name: "password", type: "password", label: "Password", required: true },
+				];
+
+			case "user":
+				return [
+					{ name: "username", type: "text", label: "Username", required: true },
+					{ name: "email", type: "email", label: "Email", required: true },
+					{ name: "password", type: "password", label: "Password", required: true },
+				];
+
+			case "verifier":
+				return [
+					{ name: "username", type: "text", label: "Username", required: true },
+					{ name: "email", type: "email", label: "Email", required: true },
+					{ name: "password", type: "password", label: "Password", required: true },
+				];
+
+			default:
+				return [];
+		}
+	};
+
+	const fields = getFieldsForRoles();
+
+	return <div>LoginPage</div>;
 }
 
 export default LoginPage;

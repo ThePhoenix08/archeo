80e29cacee0597e2f47f84eb82f2ff1d8792425c
Swapnil Dhamal <swapnildhamal58@gmail.com>
2025-06-22 17:20:40 +0530

    doc owner entity

--- apps/server/user/src/main/java/com/archeo/server/modules/user/models/Owner.java
+++ apps/server/user/src/main/java/com/archeo/server/modules/user/models/Owner.java
@@ -2,40 +2,35 @@
 
 package com.archeo.server.modules.user.models;
 
-import com.archeo.server.modules.common.enums.USER_ROLE;
+import com.archeo.server.modules.common.models.UsersCommon;
 import jakarta.persistence.*;
 import lombok.Data;
 import org.hibernate.annotations.CreationTimestamp;
 
 import java.sql.Timestamp;
+import java.time.LocalDate;
 
 @Entity
 @Data
-@Table(name = "users")
-public class Users {
+@Table(name = "owner")
+public class Owner {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY) // SERIAL in PostgreSQL
     private Long id;
 
-    @Column(nullable = false, length = 50)
-    private String username;
 
-    @Column(nullable = false, unique = true, length = 100)
-    private String email;
-
-    @Column(name = "password_hash", nullable = false, length = 255)
-    private String password;
-
-    @Enumerated(EnumType.STRING)
-    @Column(nullable = false, length = 20)
-    private USER_ROLE userRole = USER_ROLE.ROLE_OWNER;
+    @Column(name="full_name" , nullable = false, length = 300)
+    private String fullName;
 
 
+    @Column(name = "dob")
+    private LocalDate dob;
 
-    @Column(name = "is_active", nullable = false)
-    private boolean isActive = true;
 
+    @OneToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false, unique = true)
+    private UsersCommon user;
 
     @CreationTimestamp
     @Column(name = "created_at", updatable = false)

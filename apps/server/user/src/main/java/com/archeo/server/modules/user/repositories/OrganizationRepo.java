d35976cc9c086d65c3bd1480d21acd7b37057e78
Swapnil Dhamal <swapnildhamal58@gmail.com>
2025-06-22 21:49:17 +0530

    Organization and Owner model

--- apps/server/user/src/main/java/com/archeo/server/modules/user/repositories/OrganizationRepo.java
+++ apps/server/user/src/main/java/com/archeo/server/modules/user/repositories/OrganizationRepo.java
@@ -1,2 +1,7 @@
-package com.archeo.server.modules.user.repositories;public class OrganizationRepo {
+package com.archeo.server.modules.user.repositories;
+
+import com.archeo.server.modules.user.models.Organization;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface OrganizationRepo extends JpaRepository<Organization, Long> {
 }
--- apps/server/user/src/main/java/com/archeo/server/modules/user/repositories/OwnerRepo.java
+++ apps/server/user/src/main/java/com/archeo/server/modules/user/repositories/OwnerRepo.java
@@ -1,2 +1,7 @@
-package com.archeo.server.modules.user.repositories;public class OwnerRepo {
+package com.archeo.server.modules.user.repositories;
+
+import com.archeo.server.modules.user.models.Owner;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface OwnerRepo extends JpaRepository<Owner, Long> {
 }

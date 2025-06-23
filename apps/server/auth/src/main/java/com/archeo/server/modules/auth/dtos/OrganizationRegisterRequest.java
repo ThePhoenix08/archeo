d8bf7e3bdeee6a17f6a831395e29e7e92cbac9b9
Swapnil Dhamal <swapnildhamal58@gmail.com>
2025-06-23 19:33:02 +0530

    org and owner register request

--- apps/server/auth/src/main/java/com/archeo/server/modules/auth/dtos/OrganizationRegisterRequest.java
+++ apps/server/auth/src/main/java/com/archeo/server/modules/auth/dtos/OrganizationRegisterRequest.java
@@ -1,4 +1,4 @@
-package com.archeo.server.modules.user.dtos;
+package com.archeo.server.modules.auth.dtos;
 
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
--- apps/server/auth/src/main/java/com/archeo/server/modules/auth/dtos/OwnerRegisterRequest.java
+++ apps/server/auth/src/main/java/com/archeo/server/modules/auth/dtos/OwnerRegisterRequest.java
@@ -1,5 +1,9 @@
 package com.archeo.server.modules.auth.dtos;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Past;
+import jakarta.validation.constraints.Size;
 import lombok.Data;
 
 import java.time.LocalDate;
@@ -7,12 +11,22 @@ import java.time.LocalDate;
 @Data
 public class OwnerRegisterRequest {
 
+    @NotBlank(message = "Username is mandatory")
+    @Size(min = 4, max = 30, message = "Username must be between 4 and 12 characters")
     private String username;
+
+    @Email(message = "Email should be valid")
+    @NotBlank(message = "Email is required")
     private String email;
+
+    @NotBlank(message = "Password is required")
+    @Size(min = 8, message = "Password must be at least 8 characters")
     private String password;
 
-
+    @NotBlank(message = "Full name is required")
     private String fullName;
+
+    @Past(message = "Date of birth must be in the past")
     private LocalDate dob;
 
 }

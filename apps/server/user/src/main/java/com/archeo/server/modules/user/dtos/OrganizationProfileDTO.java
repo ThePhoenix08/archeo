deaee50e10456f1383316baf17876685ea073799
Swapnil Dhamal <swapnildhamal58@gmail.com>
2025-06-24 21:05:18 +0530

    org profile DTO

--- apps/server/user/src/main/java/com/archeo/server/modules/user/dtos/OrganizationProfileDTO.java
+++ apps/server/user/src/main/java/com/archeo/server/modules/user/dtos/OrganizationProfileDTO.java
@@ -1,13 +1,14 @@
 package com.archeo.server.modules.user.dtos;
 
-import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
-import lombok.NoArgsConstructor;
 
 @Data
 @Builder
-@NoArgsConstructor
-@AllArgsConstructor
-public class OrganizationProfileUpdateRequest {
+
+public class OrganizationProfileDTO {
+
+
+
+
 }

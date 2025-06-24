0110c1d8695bc73e6387530f170004f371152764
Swapnil Dhamal <swapnildhamal58@gmail.com>
2025-06-24 20:57:59 +0530

    link preview

--- apps/server/auth/src/main/java/com/archeo/server/modules/auth/controllers/LinkPreviewController.java
+++ apps/server/auth/src/main/java/com/archeo/server/modules/auth/controllers/LinkPreviewController.java
@@ -1,2 +1,26 @@
-package com.archeo.server.modules.auth.controllers;public class LinkPreviewController {
+package com.archeo.server.modules.auth.controllers;
+
+import com.archeo.server.modules.auth.dtos.LinkPreviewRequest;
+import com.archeo.server.modules.auth.models.LinkPreviewResponse;
+import com.archeo.server.modules.auth.services.LinkPreviewService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/auth/link-preview")
+@RequiredArgsConstructor
+public class LinkPreviewController {
+
+    private final LinkPreviewService linkPreviewService;
+
+    @PostMapping
+    public ResponseEntity<LinkPreviewResponse> preview(@Valid @RequestBody LinkPreviewRequest request) {
+        LinkPreviewResponse response = linkPreviewService.fetchPreview(request);
+        return ResponseEntity.ok(response);
+    }
 }

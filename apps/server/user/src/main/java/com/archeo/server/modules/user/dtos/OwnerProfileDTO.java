ba602059ffcbc62a087c6d24f12656331a051e88
Swapnil Dhamal <swapnildhamal58@gmail.com>
2025-06-24 21:06:08 +0530

    owner profile DTO

--- apps/server/user/src/main/java/com/archeo/server/modules/user/dtos/OwnerProfileDTO.java
+++ apps/server/user/src/main/java/com/archeo/server/modules/user/dtos/OwnerProfileDTO.java
@@ -6,9 +6,29 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.time.LocalDate;
+
 @Data
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
-public class OwnerProfileUpdateRequest {
+public class OwnerProfileDTO {
+
+    private String username;
+
+    private String email;
+
+    private String fullName;
+
+    private String phoneNumber;
+
+    private LocalDate dob;
+
+    private String address;
+
+    private String avatarUrl;
+
+    private String bio;
+
+    private LocalDate updatedAt;
 }

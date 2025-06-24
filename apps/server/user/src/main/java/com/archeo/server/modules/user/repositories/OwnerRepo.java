77551ca24ce0ab5b9d7b980fcfece4f0457fd6f8
Swapnil Dhamal <swapnildhamal58@gmail.com>
2025-06-24 21:08:13 +0530

    owner repo

--- apps/server/user/src/main/java/com/archeo/server/modules/user/repositories/OwnerRepo.java
+++ apps/server/user/src/main/java/com/archeo/server/modules/user/repositories/OwnerRepo.java
@@ -1,9 +1,13 @@
 package com.archeo.server.modules.user.repositories;
 
+import com.archeo.server.modules.common.models.UsersCommon;
 import com.archeo.server.modules.user.models.Owner;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface OwnerRepo extends JpaRepository<Owner, Long> {
+    Optional<Object> findByUser(UsersCommon user);
 }

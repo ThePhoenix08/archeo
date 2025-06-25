7ec6684254ae6bd2d389358bac04e8327c3f2883
Swapnil Dhamal <swapnildhamal58@gmail.com>
2025-06-25 16:14:00 +0530

    org, owner and user mapper

--- apps/server/auth/src/main/java/com/archeo/server/modules/auth/mapper/OrganizationMapper.java
+++ apps/server/auth/src/main/java/com/archeo/server/modules/auth/mapper/OrganizationMapper.java
@@ -1,2 +1,17 @@
-package com.archeo.server.modules.auth.mapper;public interface OrganizationMapper {
+package com.archeo.server.modules.auth.mapper;
+
+import com.archeo.server.modules.auth.dtos.OrganizationRegisterRequest;
+import com.archeo.server.modules.user.models.Organization;
+import org.mapstruct.*;
+
+@Mapper(componentModel = "spring")
+public interface OrganizationMapper {
+
+
+    @BeanMapping(nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
+    void mapOrganizationRegisterRequestToOrganization(OrganizationRegisterRequest organizationRegisterRequest
+                                                      , @MappingTarget Organization organization);
+
+
+
 }

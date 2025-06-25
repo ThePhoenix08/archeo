e7dd460678626c8b0babd558bb6d89218630b08a
712Kunal <jadhavkun0@gmail.com>
2025-06-25 21:07:30 +0530

    Done with the redux persist setup

--- apps/client/src/main.jsx
+++ apps/client/src/main.jsx
@@ -4,11 +4,14 @@ import "@/index.css";
 import App from "@/App.jsx";
 import { store } from "@/shared/store/store.js";
 import { Provider } from "react-redux";
+import { PersistGate } from "redux-persist/integration/react";
 
 createRoot(document.getElementById("root")).render(
 	<StrictMode>
 		<Provider store={store}>
-			<App />
+			<PersistGate loading={null} persistor={persistor}>
+				<App />
+			</PersistGate>
 		</Provider>
 	</StrictMode>
 );
--- apps/client/src/shared/store/store.js
+++ apps/client/src/shared/store/store.js
@@ -1,15 +1,40 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { apiSlice } from "@/shared/reducer/apiSlice.reducer";
 import authReducer from "@/features/auth/reducer/authSlice.reducer";
+import {
+	persistStore,
+	persistReducer,
+	FLUSH,
+	REHYDRATE,
+	PAUSE,
+	PERSIST,
+	PURGE,
+	REGISTER,
+} from "redux-persist";
+import storage from "redux-persist/lib/storage"; // defaults to localStorage for web
+
+const persistConfig = {
+	key: "root",
+	storage,
+	whitelist: ["auth"], // only auth will be persisted
+};
+
+const persistedAuthReducer = persistReducer(persistConfig, authReducer);
 
 export const store = configureStore({
 	reducer: {
+		auth: persistedAuthReducer,
 		[apiSlice.reducerPath]: apiSlice.reducer,
-		auth: authReducer,
 	},
 	middleware: (getDefaultMiddleware) =>
-		getDefaultMiddleware().concat(apiSlice.middleware),
+		getDefaultMiddleware({
+			serializableCheck: {
+				// Required for redux-persist + RTK Query to work together
+				ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+			},
+		}).concat(apiSlice.middleware),
 	devTools: process.env.NODE_ENV !== "production", // ✅ enables DevTools only in development
 });
 
+export const persistor = persistStore(store);
 export default store;

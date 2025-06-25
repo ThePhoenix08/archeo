f1f5389ced67ad4201613d79141ed8051bc63943
Swapnil Dhamal <swapnildhamal58@gmail.com>
2025-06-25 16:21:01 +0530

    Updates in org and owner repo

--- apps/server/user/src/main/java/com/archeo/server/modules/user/repositories/OwnerRepo.java
+++ apps/server/user/src/main/java/com/archeo/server/modules/user/repositories/OwnerRepo.java
@@ -3,6 +3,8 @@ package com.archeo.server.modules.user.repositories;
 import com.archeo.server.modules.common.models.UsersCommon;
 import com.archeo.server.modules.user.models.Owner;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
@@ -10,4 +12,7 @@ import java.util.Optional;
 @Repository
 public interface OwnerRepo extends JpaRepository<Owner, Long> {
     Optional<Object> findByUser(UsersCommon user);
+
+    @Query("SELECT o FROM Owner o WHERE o.user.email = :email")
+    Optional<Owner> findByUserEmail(@Param("email") String email);
 }

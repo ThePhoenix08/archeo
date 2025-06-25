17dd6904636f1b631586c9802918ab60057097f4
Drkadam07 <drkadam04@gmail.com>
2025-06-25 23:53:56 +0530

    User Setting Table model

---
+++ apps/server/user/src/main/java/com/archeo/server/modules/user/models/Settings.java
@@ -0,0 +1,39 @@
+package com.archeo.server.modules.user.models;
+
+import com.archeo.server.modules.common.models.UsersCommon;
+import com.archeo.server.modules.user.enums.NotificationType;
+import com.archeo.server.modules.user.enums.ThemePreference;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+
+@Entity
+@Table(name = "user_settings")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Settings {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @OneToOne
+    @JoinColumn(name = "user_id", nullable = false, unique = true)
+    private UsersCommon user;
+
+    @Column(length = 10)
+    private String language = "English";
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "theme_preference", length = 10)
+    private ThemePreference themePreference = ThemePreference.SYSTEM;
+
+    @Column(name = "notifications_enabled")
+    private boolean notificationsEnabled = true;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "notification_type", length = 10)
+    private NotificationType notificationType = NotificationType.IN_APP;
+}

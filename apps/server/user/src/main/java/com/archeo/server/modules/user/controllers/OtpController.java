1ad0c152566cc1ebc055aa172831dbd418ab807a
Swapnil Dhamal <swapnildhamal58@gmail.com>
2025-06-26 19:48:38 +0530

    otp controller

--- apps/server/user/src/main/java/com/archeo/server/modules/user/controllers/OtpController.java
+++ apps/server/user/src/main/java/com/archeo/server/modules/user/controllers/OtpController.java
@@ -1,2 +1,36 @@
-package com.archeo.server.modules.user.controllers;public class OtpController {
+package com.archeo.server.modules.user.controllers;
+
+import com.archeo.server.modules.user.dtos.EmailRequest;
+import com.archeo.server.modules.user.services.OtpService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/otp")
+public class OtpController {
+
+    private final OtpService otpService;
+
+    @PostMapping("/send")
+    public ResponseEntity<String> sendOtp(@RequestBody @Valid EmailRequest emailRequest){
+        otpService.sendOtp(emailRequest.getEmail());
+        return ResponseEntity.ok("OTP sent successfully to "+emailRequest.getEmail());
+    }
+
+    @PostMapping("/verify")
+    public ResponseEntity<String> verifyOtp(@RequestParam("otp") String otp,
+                                            @RequestParam("email") String email){
+        String message = otpService.verifyOtp(otp, email);
+
+        if (message.equals("Otp verified successfully")) {
+            return ResponseEntity.ok(message);
+        } else {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(message);
+        }
+    }
+
 }

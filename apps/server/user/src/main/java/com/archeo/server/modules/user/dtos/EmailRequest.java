a44004b240de9b2197c2ed0e422a22a033a474a6
Swapnil Dhamal <swapnildhamal58@gmail.com>
2025-06-26 19:49:15 +0530

    email request

--- apps/server/user/src/main/java/com/archeo/server/modules/user/dtos/EmailRequest.java
+++ apps/server/user/src/main/java/com/archeo/server/modules/user/dtos/EmailRequest.java
@@ -1,2 +1,13 @@
-package com.archeo.server.modules.user.dtos;public class EmailRequest {
+package com.archeo.server.modules.user.dtos;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import lombok.Data;
+
+@Data
+public class EmailRequest {
+
+    @NotBlank(message = "Email must not be empty")
+    @Email(message = "Invalid email format")
+    private String email;
 }
